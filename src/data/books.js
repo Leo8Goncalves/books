654d21f818aca011f1372961b54bea149dab647c
Leo8Goncalves <leandrolcg18@gmail.com>
2024-03-19 20:02:53 -0300

    newRepo

---
+++ src/data/books.js
@@ -0,0 +1,32 @@
+export const books = [
+  {
+    id: 0,
+    name: "A chama Esquecida",
+    category: "Fantasia",
+    price: 32.9,
+  },
+  {
+    id: 1,
+    name: "O Laberinto dos Sonhos",
+    category: "Fantasia",
+    price: 28.7,
+  },
+  {
+    id: 2,
+    name: "Entre o Tempo e a Eternidade",
+    category: "Romance",
+    price: 37.8,
+  },
+  {
+    id: 3,
+    name: "Sombras do Destino",
+    category: "Ação",
+    price: 45.5,
+  },
+  {
+    id: 4,
+    name: "O Último Suspiro",
+    category: "Romance",
+    price: 19.99,
+  },
+];
